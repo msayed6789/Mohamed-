9976ead024e5b6245cb34e54e16b0c6872b9a984
msayed6789 <128575094+msayed6789@users.noreply.github.com>
2023-03-30 12:54:57 +0200

    Add files via upload

---
+++ Refactor previous tasks/Data Structure/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+#define expression_Size   30
+
+
+typedef   signed short     int8_t;
+typedef   char           uint8_t;
+
+
+
+int8_t printStack(ST_stack_t *stack);
+
+#endif // MAIN_H_INCLUDED
