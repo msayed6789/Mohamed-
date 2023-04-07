0506cc15aefd5610319952555a4a30d983f97d61
msayed6789 <128575094+msayed6789@users.noreply.github.com>
2023-04-08 01:14:57 +0200

    Add files via upload

---
+++ LED Sequence V1.0/MCAL/DIO/DIO.c
@@ -0,0 +1,188 @@
+/*
+ * DIO.c
+ *
+ * Created: 4/5/2023 1:57:07 AM
+ * Author : Mohamed Sayed
+ */ 
+
+
+#include "DIO_Interface.h"
+
+extern const PIN_Status Pin_StatusArray [PINs_Total];
+
+void DIO_InitPin (PIn_name pin ,PIN_Status status)
+{
+ 
+ switch(status)
+ {
+	 case OUTPUT:
+	 if(pin/8 == 0)
+	 {
+		 Set_Bit(DDRA,pin%8);
+		 CLR_Bit(PORTA,pin%8);
+	 }
+	 else if(pin/8 == 1)
+	 {
+		 Set_Bit(DDRB,pin%8);
+		 CLR_Bit(PORTB,pin%8);
+	 }
+	 else if(pin/8 == 2)
+	 {
+		 Set_Bit(DDRC,pin%8);
+		 CLR_Bit(PORTC,pin%8);
+	 }
+	 else if(pin/8 == 3)
+	 {
+		 Set_Bit(DDRD,pin%8);
+		 CLR_Bit(PORTD,pin%8);
+	 }
+	 break;
+	 case INFREE:
+	 if(pin/8 == 0)
+	 {
+		 CLR_Bit(DDRA,pin%8);
+		 CLR_Bit(PORTA,pin%8);
+	 }
+	 else if(pin/8 == 1)
+	 {
+		 CLR_Bit(DDRB,pin%8);
+		 CLR_Bit(PORTB,pin%8);
+	 }
+	 else if(pin/8 == 2)
+	 {
+		 CLR_Bit(DDRC,pin%8);
+		 CLR_Bit(PORTC,pin%8);
+	 }
+	 else if(pin/8 == 3)
+	 {
+		 CLR_Bit(DDRD,pin%8);
+		 CLR_Bit(PORTD,pin%8);
+	 }
+	 break;
+	 case INPUT:
+	 if(pin/8 == 0)
+	 {
+		 CLR_Bit(DDRA,pin%8);
+		 Set_Bit(PORTA,pin%8);
+	 }
+	 else if(pin/8 == 1)
+	 {
+		 CLR_Bit(DDRB,pin%8);
+		 Set_Bit(PORTB,pin%8);
+	 }
+	 else if(pin/8 == 2)
+	 {
+		 CLR_Bit(DDRC,pin%8);
+		 Set_Bit(PORTC,pin%8);
+	 }
+	 else if(pin/8 == 3)
+	 {
+		 CLR_Bit(DDRD,pin%8);
+		 Set_Bit(PORTD,pin%8);
+	 }
+	 break;
+ }
+}
+
+
+
+void DIO_init (void)
+{
+	PIn_name p;
+	for (p=PINA0;p<PINs_Total;p++)
+	{
+		DIO_InitPin (p ,Pin_StatusArray[p]);
+	}
+}
+
+
+
+void DIO_WRitePin (PIn_name pin ,Voltage_type s)
+{
+	switch (s)
+	{
+		case HIGH:
+		if(pin/8 == 0)
+		{
+			Set_Bit (PORTA,pin%8);
+		}
+		else if(pin/8 == 1)
+		{
+			Set_Bit (PORTB,pin%8);
+		}
+		else if(pin/8 == 2)
+		{
+			Set_Bit (PORTC,pin%8);
+		}
+		else if(pin/8 == 3)
+		{
+			Set_Bit (PORTD,pin%8);
+		}
+		break;
+		case LOW:
+		if(pin/8 == 0)
+		{
+			CLR_Bit (PORTA,pin%8);
+		}
+		else if(pin/8 == 1)
+		{
+			CLR_Bit (PORTB,pin%8);
+		}
+		else if(pin/8 == 2)
+		{
+			CLR_Bit (PORTC,pin%8);
+		}
+		else if(pin/8 == 3)
+		{
+			CLR_Bit (PORTD,pin%8);
+		}
+		break;
+	}
+	
+}
+
+void DIO_WritePort(PORT_Type Port,u8 data)
+{
+	switch (Port)
+	{
+		case PA:
+		PORTA =data;
+		break;
+		case PB:
+		PORTB =data;
+		break;
+		case PC:
+		PORTC =data;
+		break;
+		case PD:
+		PORTD =data;
+		break;
+		
+	}
+	
+}
+
+Voltage_type DIO_ReadPin(PIn_name pin)
+{
+	Voltage_type volt=LOW;
+	if(pin/8 == 0)
+	{
+		volt=Read_Bit(PINA,pin%8);
+	}
+	else if(pin/8 == 1)
+	{
+		volt=Read_Bit(PINB,pin%8);
+	}
+	else if(pin/8 == 2)
+	{
+		volt=Read_Bit(PINC,pin%8);
+	}
+	else if(pin/8 == 3)
+	{
+		volt=Read_Bit(PIND,pin%8);
+	}
+	return volt;
+	
+}
+
+
---
+++ LED Sequence V1.0/MCAL/DIO/DIO_Lcfg.c
@@ -0,0 +1,47 @@
+/*
+ * DIO_Lcfg.c
+ *
+ * Created: 4/5/2023 1:57:07 AM
+ * Author : Mohamed Sayed
+ */ 
+
+
+#include "DIO_INTERFACE.h"
+
+
+
+const PIN_Status Pin_StatusArray [PINs_Total]={
+	
+OUTPUT,      //PINA0
+OUTPUT,		 //PINA1
+OUTPUT,		 //PINA2
+OUTPUT,		 //PINA3
+OUTPUT,      //PINA4
+OUTPUT,		 //PINA5
+OUTPUT,		 //PINA6
+OUTPUT,      //PINA7
+INPUT,      //PINB0
+INPUT,		 //PINB1
+INPUT,		 //PINB2
+INPUT,		 //PINB3
+INPUT,      //PINB4  //SS
+INPUT,		 //PINB5 //MOSI
+INPUT,		 //PINB6  //MISO
+INPUT,      //PINB7 //SCK
+INPUT,      //PINC0
+OUTPUT,		 //PINC1
+OUTPUT,		 //PINC2
+OUTPUT,		 //PINC3
+OUTPUT,      //PINC4
+OUTPUT,		 //PINC5
+OUTPUT,		 //PINC6
+OUTPUT,      //PINC7
+INPUT,      //PIND0
+INFREE,		 //PIND1
+INFREE,		 //PIND2
+INFREE,		 //PIND3
+INFREE,      //PIND4
+INFREE,		 //PIND5
+INFREE,		 //PIND6
+INFREE       //PIND7
+};
