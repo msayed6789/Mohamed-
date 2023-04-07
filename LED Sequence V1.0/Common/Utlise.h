c1bea50532e26938333565a4340d730a4b564a24
msayed6789 <128575094+msayed6789@users.noreply.github.com>
2023-04-08 01:19:02 +0200

    Add files via upload

---
+++ LED Sequence V1.0/Common/Utlise.h
@@ -0,0 +1,17 @@
+
+#ifndef UTILS_H_
+#define UTILS_H_
+
+
+#include "Standard_Type.h"
+
+
+#define Read_Bit(reg,bit)         ((reg>>bit) & 1)
+#define Set_Bit(reg,bit)          (reg|=1<<bit)
+#define CLR_Bit(reg,bit)          (reg&=~(1<<bit))
+#define TOG_BIT(reg,bit)          (reg=reg^(1<<bit))
+
+
+
+
+#endif /* UTILS_H_ */
