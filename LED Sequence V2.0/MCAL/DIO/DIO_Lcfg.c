e9f53f6e3d87e56d59cdf63a757a24d6d36a697a
msayed6789 <128575094+msayed6789@users.noreply.github.com>
2023-04-08 01:59:20 +0200

    Add files via upload

---
+++ LED Sequence V2.0/MCAL/DIO/DIO_Lcfg.c
@@ -0,0 +1,39 @@
+#include "DIO_INTERFACE.h"
+
+
+
+const PIN_Status Pin_StatusArray [PINs_Total]={
+	
+OUTPUT,      //PINA0
+OUTPUT,		 //PINA1
+OUTPUT,		 //PINA2
+OUTPUT,		 //PINA3
+OUTPUT,      //PINA4
+OUTPUT,		 //PINA5
+OUTPUT,		 //PINA6
+OUTPUT,      //PINA7
+OUTPUT,      //PINB0
+OUTPUT,		 //PINB1
+OUTPUT,		 //PINB2
+OUTPUT,		 //PINB3
+OUTPUT,      //PINB4  //SS
+OUTPUT,		 //PINB5 //MOSI
+OUTPUT,		 //PINB6  //MISO
+OUTPUT,      //PINB7 //SCK
+INPUT,      //PINC0
+OUTPUT,		 //PINC1
+OUTPUT,		 //PINC2
+OUTPUT,		 //PINC3
+OUTPUT,      //PINC4
+OUTPUT,		 //PINC5
+OUTPUT,		 //PINC6
+OUTPUT,      //PINC7
+INPUT,      //PIND0
+INPUT,		 //PIND1
+INPUT,		 //PIND2
+INPUT,		 //PIND3
+INPUT,      //PIND4
+INPUT,		 //PIND5
+INPUT,		 //PIND6
+INPUT       //PIND7
+};
