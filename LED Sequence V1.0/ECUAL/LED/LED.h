fc177751985d494522490069b728835d5e09f541
msayed6789 <128575094+msayed6789@users.noreply.github.com>
2023-04-08 01:16:04 +0200

    Add files via upload

---
+++ LED Sequence V1.0/ECUAL/LED/LED.h
@@ -0,0 +1,24 @@
+/*
+ * LED.h
+ *
+ * Created: 4/5/2023 2:01:12 AM
+ *  Author: AIO
+ */ 
+
+
+#ifndef LED_H_
+#define LED_H_
+#include "DIO_Interface.h"
+/***************************LED Configuration******************************************************/
+#define  LED1      PINA0
+#define  LED2      PINA1
+#define  LED3      PINA2
+#define  LED4      PINA3
+#define  LED5      PINA4
+#define  LED6      PINA5
+/*************************************************************************************************/
+void LED_ON (u8 LED );
+void LED_OFF (u8 LED );
+
+
+#endif /* LED_H_ */
