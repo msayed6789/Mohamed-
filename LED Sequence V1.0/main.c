3932216b467339457d750bdaf89cd215aff1fe64
msayed6789 <128575094+msayed6789@users.noreply.github.com>
2023-04-08 01:19:42 +0200

    Add files via upload

---
+++ LED Sequence V1.0/main.c
@@ -0,0 +1,17 @@
+/*
+ * LED sequence V1.0.c
+ *
+ * Created: 4/5/2023 1:57:07 AM
+ * Author : Mohamed Sayed
+ */ 
+#include "App.h"
+
+int main(void)
+{
+	APP_Init();
+    while (1) 
+    {
+		APP_Start();
+    }
+}
+
