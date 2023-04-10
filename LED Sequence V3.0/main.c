b89a6a3202baf02ae4c68f74700f1a98c09ac739
msayed6789 <128575094+msayed6789@users.noreply.github.com>
2023-04-10 17:28:48 +0200

    Add files via upload

---
+++ LED Sequence V3.0/main.c
@@ -0,0 +1,20 @@
+/*
+ * LED Sequence V2.0.c
+ *
+ * Created: 4/5/2023 3:18:19 PM
+ * Author : AIO
+ */ 
+
+#include "App.h"
+
+
+int main(void)
+{
+    APP_Init();
+    while (1) 
+    {
+		APP_Start();
+    }
+}
+
+
