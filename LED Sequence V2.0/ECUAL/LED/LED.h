19b8a8f731ef513fd21a306d5fb540be69bfa0d4
msayed6789 <128575094+msayed6789@users.noreply.github.com>
2023-04-13 14:12:46 +0200

    Update LED.h

--- LED Sequence V2.0/ECUAL/LED/LED.h
+++ LED Sequence V2.0/ECUAL/LED/LED.h
@@ -17,29 +17,7 @@
 #define  LED5      PINA4
 #define  LED6      PINA5
 /*************************************************************************************************/
-/*
-typedef enum {
-	LED_1=LED1,
-	LED_2,
-	LED_3,
-	LED_4,
-	LED_5,
-	LED_6,
-	LED_Total
-}LED_Num;
-void LED_init1All(void);      //The LED PINS must be behind each other  
-void LED_init(LED_Num LED);
-*/
-typedef enum{
-	blank_1,
-	blank_2,
-	blank_3,
-	blank_4,
-	blank_5
-}Blank_Type;
-void LED_Blank(Blank_Type Blank);
-void LED_StopBlank(void);
-//void blank(void);
+/
 void LED_ON (u8 LED );
 void LED_OFF (u8 LED );
 
