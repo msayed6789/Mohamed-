13886e416d4e8ceb5b10a02375b7382984dc29bc
msayed6789 <128575094+msayed6789@users.noreply.github.com>
2023-05-14 15:46:45 +0300

    Add files via upload

---
+++ pre-configuration and linking configuration file for the UART driver/Preprocessor configuration/uart_Pcfg.c
@@ -0,0 +1,67 @@
+/*
+ * uart_Pcfg.c
+ *
+ * Created: 5/10/2023 12:47:55 PM
+ *  Author: Mohamed Sayed 
+ */ 
+
+//#include "uart_Pcfg.h"
+
+/*****************************************The Defination of Baudrate**********************************************************************************************/
+typedef enum {
+	Baudrate2400,
+	Baudrate4800,
+	Baudrate9600,
+	Baudrate144K,
+	Baudrate_Total
+}Uart_BaudRate_en;
+
+#define Uart_BaudRate   Baudrate9600
+/****************************************The uart speed************************************************************************************************************/
+typedef enum {
+	Normal_Speed,
+	Double_Speed,
+	Speed_Total
+}Uart_Speed_en;
+
+#define  Uart_Speed   Normal_Speed
+/***************************************The uart mode**************************************************************************************************************/
+typedef enum{
+	Asynchronous,
+	Synchronous
+}Uart_Mode_en;
+
+#define  Uart_Mode    Asynchronous
+/***************************************Parity bit******************************************************************************************************************/
+typedef enum{
+	Even,
+	Odd,
+	No_Parity
+}Uart_Parity_en;
+
+#define   Uart_Parity  Even
+/*************************************Stop bit**********************************************************************************************************************/
+typedef enum{
+	StopBit_1,
+	StopBit_2
+}Uart_StopBit_en;
+
+ #define   Uart_StopBit   StopBit_1
+/***********************************Number of data bit**************************************************************************************************************/
+typedef enum{
+	DataBit_5,
+	DataBit_6,
+	DataBit_7,
+	DataBit_8,
+	DataBit_9
+}Uart_DataBit_en;
+
+#define   Uart_DataBit   DataBit_8 
+/**********************************Enable for transmitter & reciever************************************************************************************************/
+typedef enum{
+	Transmitter,
+	Reciever,
+	TR
+}Uart_Enable_en;
+
+#define  Uart_Enable   Transmitter
